4fb1309a3dab85d818b990975a87250107a127d5
aownabbas <aaown344@gmail.com>
2024-12-22 09:33:26 +0500

    fix build

--- src/Utlits/projectList.jsx
+++ src/Utlits/projectList.jsx
@@ -1,16 +1,15 @@
 // import pro1 from "../assets/img/project/pro1.png";
-import machkosher from '../assets/img/project/machkosher2.png';
+// import machkosher from '../assets/img/project/machkosher2.PNG';
 import pro3 from '../assets/img/project/pro3.png';
 import pro4 from '../assets/img/project/pro4.png';
 import pro5 from '../assets/img/project/pro5.png';
 import pro6 from '../assets/img/project/pro6.png';
-import cogeter from '../assets/img/project/cogeter.png';
-import machkosher_web from '../assets/img/project/machkosher1.png';
-import dialink from '../assets/img/project/dialink.png';
-import people_plus from '../assets/img/project/people.png';
-import ebarzaar from '../assets/img/project/ebazaar.png';
-import dentalId_landPage from '../assets/img/project/dental1.png';
-
+import cogeter from '../assets/img/project/cogeter.PNG';
+// import machkosher_web from '../assets/img/project/machkosher1.png';
+// import dialink from '../assets/img/project/dialink.png';
+// import people_plus from '../assets/img/project/people.png';
+// import ebarzaar from '../assets/img/project/ebazaar.png';
+// import dentalId_landPage from '../assets/img/project/dental1.png';
 
 export const projectList = [
   {
@@ -21,49 +20,50 @@ export const projectList = [
     category: ['branding_app', 'website', 'all'],
   },
 
-  {
-    id: 4,
-    image: machkosher_web,
-    heading: 'Machkosher Website',
-    subHeading: 'Website Development',
-    category: ['website', '', 'all'],
-  },
+  // {
+  //   id: 4,
+  //   image: machkosher_web,
+  //   heading: 'Machkosher Website',
+  //   subHeading: 'Website Development',
+  //   category: ['website', '', 'all'],
+  // },
 
-  {
-    id: 5,
-    image: dialink,
-    heading: 'Dialink Admin Dashboard',
-    subHeading: 'Website Development',
-    category: ['website', 'landing_page', 'all'],
-  },
-  {
-    id: 2,
-    image: ebarzaar,
-    heading: 'E-Bazaar Online Store',
-    subHeading: 'Website Development',
-    category: ['branding_app', 'website', 'all'],
-  },
-  {
-    id: 3,
-    image: machkosher,
-    heading: 'Machkosher Dashboard',
-    subHeading: 'Website Development',
-    category: ['los_app', 'landing_page', 'all'],
-  },
-  {
-    id: 6,
-    image: people_plus,
-    heading: 'PeoplePlus',
-    subHeading: 'Website Development',
-    category: ['', 'website', 'all'],
-  },
-  {
-    id: 7,
-    image: dentalId_landPage,
-    heading: 'dentalId landing Page',
-    subHeading: 'Website Development',
-    category: ['landing_page', 'website', 'all'],
-  },
+  // {
+  //   id: 5,
+  //   image: dialink,
+  //   heading: 'Dialink Admin Dashboard',
+  //   subHeading: 'Website Development',
+  //   category: ['website', 'landing_page', 'all'],
+  // },
+  // {
+  //   id: 2,
+  //   image: ebarzaar,
+  //   heading: 'E-Bazaar Online Store',
+  //   subHeading: 'Website Development',
+  //   category: ['branding_app', 'website', 'all'],
+  // },
+  // {
+  //   id: 3,
+  //   image: machkosher,
+  //   heading: 'Machkosher Dashboard',
+  //   subHeading: 'Website Development',
+  //   category: ['los_app', 'landing_page', 'all'],
+  // },
+  // {
+  //   id: 6,
+  //   image: people_plus,
+  //   heading: 'PeoplePlus',
+  //   subHeading: 'Website Development',
+  //   category: ['', 'website', 'all'],
+  // },
+  // {
+  //   id: 7,
+  //   image: dentalId_landPage,
+  //   heading: 'dentalId landing Page',
+  //   subHeading: 'Website Development',
+  //   category: ['landing_page', 'website', 'all'],
+  // },
 ];
 
-export const imagesList = [cogeter, ebarzaar, machkosher, machkosher_web, people_plus, dialink];
+// export const imagesList = [cogeter, ebarzaar, machkosher, machkosher_web, people_plus, dialink];
+export const imagesList = [cogeter];
