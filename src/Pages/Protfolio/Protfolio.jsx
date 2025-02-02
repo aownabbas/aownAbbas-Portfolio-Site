19e0bcfd8c649fc1fd25a31fe44a114143fcfa8e
aownabbas <aaown344@gmail.com>
2025-02-02 08:38:02 +0500

    fix: issues for wrong image opening is resolved

--- src/Pages/Protfolio/Protfolio.jsx
+++ src/Pages/Protfolio/Protfolio.jsx
@@ -77,7 +77,7 @@ const Protfolio = () => {
                   image={image}
                   heading={heading}
                   subHeading={subHeading}
-                  openLightbox={openLightbox}
+                  // openLightbox={openLightbox}
                   index={index}
                   navigate="/protfolio-details"
                 />
